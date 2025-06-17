0d3a7d4f5ede3fc44ab602736567ed09e4b76635
Sophie Luna Schumann <me@luna.vg>
2025-06-17 03:19:33 +0200

    Update CoreDNS version to v1.12.2 and some more minor improvements and error-feedback

--- coredns/rootfs/etc/cont-init.d/config.sh
+++ coredns/rootfs/etc/cont-init.d/config.sh
@@ -5,7 +5,21 @@
 
 CONFIG="/etc/corefile.conf"
 bashio::log.info "Configuring CoreDNS..."
-tempio \
+if ! tempio \
     -conf /data/options.json \
     -template /usr/share/tempio/coredns.config \
-    -out "${CONFIG}"
+    -out "${CONFIG}"; then
+    bashio::log.error "Failed to generate CoreDNS configuration"
+    exit 1
+fi
+
+# Validate the generated config
+if [[ ! -f "${CONFIG}" ]]; then
+    bashio::log.error "Configuration file was not created"
+    exit 1
+fi
+if ! coredns -conf "${CONFIG}" -validate; then
+    bashio::log.error "CoreDNS configuration validation failed"
+    exit 1
+fi
+bashio::log.info "CoreDNS configuration is valid"
